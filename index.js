7eb84f4eefb89e26e93964220a78686dba78f54f
Jediprodap <55718816+Jediprodap@users.noreply.github.com>
2019-09-23 18:47:56 -0300

    teste link gabi

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ const slides = [
   {
     name: 'Jetsons Farm',
     page:
-      'https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSfkUzbPxmPQA49Az1tXINQ6MpfOHVq7rFthvikqkNIVADKThgOVCSrue_WEbOuBZ8i1M5L5FP9FjZV/pubhtml?gid=681837777&single=true',
-    seconds: 20
-  },
-   {
-    name: 'Racha Cuca',
-    page:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQvcXuN_MWoenkykYHf4FEa832STbgvAqV_Tc06Rn6ToSAAl6TXHSvOR4Mw3X7Md-1k-KJ05yFBJaTF/pubhtml?gid=1082556539&single=true',
-    seconds: 20
-  },
-  {
-    name: 'Racha Cuca',
-    page:
-      'https://drive.google.com/file/d/1VqVGF7rPBOQ7HXKY_C8Bhe63Zab9MwX0/view',
+      'https://www.google.com/url?q=https://docs.google.com/presentation/d/e/2PACX-1vQQLb7QDeWEM7Uy4a4Bd7cI--YFQtLNQPJz5mIgnvdZ2NR94mPPbXQXkN4HEkc6f8nOaXmb_2oqTqfy/pub?start%3Dtrue%26loop%3Dfalse%26delayms%3D60000&sa=D&source=hangouts&ust=1569361643150000&usg=AFQjCNFFpa8WzAuRq3RMkW19MnmzOzlBtQ',
     seconds: 20
   }
 ];
